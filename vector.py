af525e9d5675de1f8974edf1ba93be30366fdbb5
Vincent Lin <2vincentlin@gmail.com>
2017-09-07 04:18:20 +0800

    add plus, minus, times_scalar, magnitude, normalized

--- vector.py
+++ vector.py
@@ -1,9 +1,12 @@
+from math import sqrt
+
 class Vector(object):
     def __init__(self, coordinates):
         try:
             if not coordinates:
                 raise ValueError
             self.coordinates = tuple(coordinates)
+            # tuple will raise a TypeError if coordinates is not iterable
             self.dimension = len(coordinates)
 
         except ValueError:
@@ -11,6 +14,40 @@ class Vector(object):
 
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
+            
+    def plus(self, v):
+        try:
+            if self.dimension != v.dimension:
+                raise ValueError
+        except ValueError:
+            raise ValueError('Length is not equal')
+            
+        new_coordinates= [x+y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)
+    
+    def minus(self, v):
+        try:
+            if self.dimension != v.dimension:
+                raise ValueError
+        except ValueError:
+            raise ValueError('Length is not equal')
+            
+        new_coordinates= [x-y for x, y in zip(self.coordinates, v.coordinates)]
+        return Vector(new_coordinates)     
+
+    def times_scalar(self, c):
+        new_coordinates= [c*x for x in self.coordinates]
+        return Vector(new_coordinates)
+    
+    def magnitude(self):
+        coordinates_squared= [x**2 for x in self.coordinates]
+        return sqrt(sum(coordinates_squared))
+    
+    def normalized(self):
+        try:
+            return self.times_scalar(1./self.magnitude())
+        except ZeroDivisionError:
+            raise ZeroDivisionError('Cannot normalize the zero vector')
 
 
     def __str__(self):
@@ -20,7 +57,7 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
-test= Vector([1,2,3])
-print test
-test1= Vector([2,3,4])
-print test == test1
+test= Vector([0])
+
+print test.normalized()
+
