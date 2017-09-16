1e7d09f00c09d53a54490223c4f4b646605dccc9
Vincent Lin <2vincentlin@gmail.com>
2017-09-17 05:14:13 +0800

    add triangular and rref function

--- linsys.py
+++ linsys.py
@@ -24,6 +24,69 @@ class LinearSystem(object):
 
         except AssertionError:
             raise Exception(self.ALL_PLANES_MUST_BE_IN_SAME_DIM_MSG)
+            
+            
+    def compute_rref(self):
+        tf= self.compute_triangular_form()
+        
+        # if the system has less equation than dimension, or other way around,
+        # it may cause index out of range
+        if self.dimension > len(self):
+            n= len(self)
+        else:
+            n= self.dimension
+        
+        # all coefficient of pivot go to 1
+        for i in range(n):
+            if not MyDecimal(tf[i].normal_vector.coordinates[i]).is_near_zero():
+                coe= Decimal('1.0')/tf[i].normal_vector.coordinates[i]
+                tf.multiply_coefficient_and_row(coe, i)
+        
+        # this loop perform elimination from bottom to top        
+        for i in range(n - 1, 0, -1):
+            # loop from [n -1, ... ,2,1]
+            for j in range(i-1, -1, -1):
+                # loop from [n-1,..., 1,0]
+                if not MyDecimal(tf[i].normal_vector.coordinates[i]).is_near_zero():
+                    # this check may not be necessary because impossible to have ZeroDivisionError
+
+                    coe= tf[j].normal_vector.coordinates[i]
+                    tf.add_multiple_times_row_to_row(-coe, i, j)      
+                            
+        return tf
+            
+    def compute_triangular_form(self):
+        system= deepcopy(self)
+        
+
+        for i in range(len(system)-1):
+            
+            # this loop will swap equations when its pivot = 0 to next (downward)
+            # equation whose variable on the same position is not 0            
+            for j in range(system.dimension):
+                if (i == j) and (MyDecimal(system[i].normal_vector.coordinates[i]).is_near_zero()):
+                    temp= i+1
+                    while temp < len(system):
+                        #print system[temp].normal_vector.coordinates[j]
+                        if system[temp].normal_vector.coordinates[i] != 0:
+                            system.swap_rows(i, temp)
+                            break
+                        temp += 1
+                        
+            #print 'after swap: \n', system
+
+            # this loop will perform elimination downward for each i
+            for j in range(i+1, len(system)):
+                #print 'i, j: ', i, j
+                
+                # when there are two equal equations in the system, will have ZeroDivisionError
+                if not MyDecimal(system[i].normal_vector.coordinates[i]).is_near_zero():
+                    #print system[i].normal_vector.coordinates[i], system[j].normal_vector.coordinates[i]
+                    coe= system[j].normal_vector.coordinates[i]/system[i].normal_vector.coordinates[i]
+                    system.add_multiple_times_row_to_row(-coe, i, j)
+
+
+        return system
 
 
     def swap_rows(self, row1, row2):
@@ -33,17 +96,19 @@ class LinearSystem(object):
     def multiply_coefficient_and_row(self, coefficient, row):
         new_n= self[row].normal_vector.times_scalar(coefficient)
         new_k= self[row].constant_term*coefficient
-        self.planes[row]= Plane(normal_vector=new_n, constant_term= new_k)
+        self[row]= Plane(normal_vector=new_n, constant_term= new_k)
         pass # add your code here
 
 
     def add_multiple_times_row_to_row(self, coefficient, row_to_add, row_to_be_added_to):
-        add_vector= self.planes[row_to_add].normal_vector.times_scalar(coefficient)
-        add_term= self.planes[row_to_add].constant_term*coefficient
-        new_vector= self.planes[row_to_be_added_to].normal_vector.plus(add_vector)
-        new_constant= self.planes[row_to_be_added_to].constant_term + add_term
-        self.planes[row_to_be_added_to]= Plane(new_vector, new_constant)
+        add_vector= self[row_to_add].normal_vector.times_scalar(coefficient)
+        add_term= self[row_to_add].constant_term*coefficient
+        new_vector= self[row_to_be_added_to].normal_vector.plus(add_vector)
+        new_constant= self[row_to_be_added_to].constant_term + add_term
+        self[row_to_be_added_to]= Plane(new_vector, new_constant)
         pass # add your code here
+        
+
 
 
     def indices_of_first_nonzero_terms_in_each_row(self):
@@ -95,26 +160,47 @@ class MyDecimal(Decimal):
         return abs(self) < eps
 
 
-p0 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
-p1 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
-p2 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
-p3 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
 
-s = LinearSystem([p0,p1,p2,p3])
+p0 = Plane(normal_vector=Vector(['1','2','3']), constant_term='1')
+p1 = Plane(normal_vector=Vector(['4','5','6']), constant_term='4')
+p2 = Plane(normal_vector=Vector(['7','8','4']), constant_term='8')
 
-print s.indices_of_first_nonzero_terms_in_each_row()
-print '{},{},{},{}'.format(s[0],s[1],s[2],s[3])
-print len(s)
-print s
+s = LinearSystem([p0,p1,p2])
 
-s.indices_of_first_nonzero_terms_in_each_row()
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='-1') and
+        r[1] == p2):
+    print 'test case 1 failed'
 
-s[0] = p1
-print s
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1','1','1']), constant_term='2')
+s = LinearSystem([p1,p2])
+r = s.compute_rref()
+if not (r[0] == p1 and
+        r[1] == Plane(constant_term='1')):
+    print 'test case 2 failed'
 
-print MyDecimal('1e-9').is_near_zero()
-print MyDecimal('1e-11').is_near_zero()
+p1 = Plane(normal_vector=Vector(['1','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['0','1','0']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1','1','-1']), constant_term='3')
+p4 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
+s = LinearSystem([p1,p2,p3,p4])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term='0') and
+        r[1] == p2 and
+        r[2] == Plane(normal_vector=Vector(['0','0','-2']), constant_term='2') and
+        r[3] == Plane()):
+    print 'test case 3 failed'
 
-print 'test start \n'
-
-
+p1 = Plane(normal_vector=Vector(['0','1','1']), constant_term='1')
+p2 = Plane(normal_vector=Vector(['1','-1','1']), constant_term='2')
+p3 = Plane(normal_vector=Vector(['1','2','-5']), constant_term='3')
+s = LinearSystem([p1,p2,p3])
+r = s.compute_rref()
+if not (r[0] == Plane(normal_vector=Vector(['1','0','0']), constant_term=Decimal('23')/Decimal('9')) and
+        r[1] == Plane(normal_vector=Vector(['0','1','0']), constant_term=Decimal('7')/Decimal('9')) and
+        r[2] == Plane(normal_vector=Vector(['0','0','1']), constant_term=Decimal('2')/Decimal('9'))):
+    print 'test case 4 failed'
